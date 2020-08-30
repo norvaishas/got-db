3fecb50f60c078f75f569c72289408c2b3bce35a
Sergei <snorvaishas@yandex.ru>
2020-08-30 12:13:49 +0300

    get and use data from api in a component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,24 +1,12 @@
 import React, {Component} from 'react';
-import GotService from '../../services/got-service';
 import Header from '../header/header';
 import RandomChar from '../random-char/random-char';
 import ItemList from '../item-list/item-list';
 import CharDetails from '../char-details/char-details';
-import {Col, Row, Container} from 'reactstrap'
-
-/*
-
-const got = new GotService();
-
-got.getAllCharacters()
-  .then(body => body.forEach(elem => console.log(elem.name)))
-
-got.getSomeCharacter(583)
-  .then(body => console.log(body.name))
-
-*/
+import {Col, Row, Container} from 'reactstrap';
 
 export default class App extends Component {
+
     render() {
         return (
           <>
--- src/components/random-char/random-char.jsx
+++ src/components/random-char/random-char.jsx
@@ -1,31 +1,60 @@
-import React from 'react';
-import './random-char.css'
+import React, {Component} from 'react';
+import './random-char.css';
+import GotService from '../../services/got-service';
 
+const some = new GotService();
+some.getAllHouses().then(qwe => console.log(qwe))
 
-const RandomChar = () => {
-    return (
-      <div className="random-block rounded">
-          <h4>Random Character: John</h4>
-          <ul className="list-group list-group-flush">
-              <li className="list-group-item d-flex justify-content-between">
-                  <span className="term">Gender </span>
-                  <span>male</span>
-              </li>
-              <li className="list-group-item d-flex justify-content-between">
-                  <span className="term">Born </span>
-                  <span>11.03.1039</span>
-              </li>
-              <li className="list-group-item d-flex justify-content-between">
-                  <span className="term">Died </span>
-                  <span>13.09.1089</span>
-              </li>
-              <li className="list-group-item d-flex justify-content-between">
-                  <span className="term">Culture </span>
-                  <span>Anarchy</span>
-              </li>
-          </ul>
-      </div>
-    )
-}
+export default class RandomChar extends Component {
 
-export default RandomChar;
+    constructor() {
+        super();
+        this.updateCharacter();
+    }
+
+    got = new GotService();
+
+    state = {
+        name: null,
+        gender: null,
+        born: null,
+        died: null,
+        culture: null,
+    };
+
+    updateCharacter = () => {
+        const id = Math.floor(Math.random() * 2000 + 25);
+        this.got.getCharacter(id)
+          .then(char => {
+              this.setState(char);
+          })
+          .catch(error => console.log(error));
+    };
+
+    render() {
+        const {name, gender, born, died, culture} = this.state;
+        return (
+          <div className="random-block rounded">
+              <h4>Random Character: <span className='main'>{name}</span></h4>
+              <ul className="list-group list-group-flush">
+                  <li className="list-group-item d-flex justify-content-between">
+                      <span className="term">Gender </span>
+                      <span>{gender}</span>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between">
+                      <span className="term">Born </span>
+                      <span>{born}</span>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between">
+                      <span className="term">Died </span>
+                      <span>{died}</span>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between">
+                      <span className="term">Culture </span>
+                      <span>{culture}</span>
+                  </li>
+              </ul>
+          </div>
+        )
+    }
+};
